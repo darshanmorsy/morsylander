c44d5f7ff6dbeca1ba3325c3948e4d55d70fc74d
darshanmorsy <129140768+darshanmorsy@users.noreply.github.com>
2023-09-13 13:59:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cron = require('node-cron');
 
 // app.use(cors(corsOptions));
 app.use(cors({
-    origin: '*'
+    origin: 'https://talented-spacesuit-jay.cyclic.app'
 }));
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
