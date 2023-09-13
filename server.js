b29f95c9fc868137215da7d781dd6bc8605fec3b
darshanmorsy <129140768+darshanmorsy@users.noreply.github.com>
2023-09-13 14:04:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ const cron = require('node-cron');
 app.use(cors({
     origin: 'https://morsypropertydealer.com/'
 }));
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 // Replace <YourMongoDBURI> with your MongoDB connection URI
